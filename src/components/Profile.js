2bfdcb892cdad538d06965a986f75c9e33790dd2
srosenberg74 <srosenberg@alphaworks.tech>
2022-04-28 19:29:42 -0700

    styles(portfolio): add overthetop style shadows

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,31 +1,36 @@
 import React from "react";
 import Sam from "../images/Sam.jpg";
+import "../App.css";
 
 function Profile() {
   return (
     <div className="profile">
       <div>
-        <img
-          src={Sam}
-          alt={""}
-          className="img-fluid d-md-none border rounded-1 me-4 mt-2"
-        />
-        <img
-          src={Sam}
-          alt={""}
-          className="w-50 float-md-start d-none d-md-block mb-2 me-4 mt-2"
-        />
+          <img
+            src={Sam}
+            alt={""}
+            className="img-fluid d-md-none border rounded-1 me-4 mt-2"
+          />
+          <img
+            src={Sam}
+            alt={""}
+            className="w-50 float-md-start d-none d-md-block mb-2 me-4 mt-2"
+          />
       </div>
       <div className="mb-2">
         <h1
           className="my-3 fs-1"
-          style={{color: "#082c45a8", fontFamily: "PT Serif", scrollPadding: "70px"}}
+          style={{
+            color: "#082c45a8",
+            fontFamily: "PT Serif",
+            scrollPadding: "70px",
+          }}
         >
           Profile
         </h1>
         <p
           className="fs-5 fw-bold"
-          style={{lineHeight: 1.4, color: "#082c45d7", fontFamily: "raleway"}}
+          style={{ lineHeight: 1.4, color: "#082c45d7", fontFamily: "raleway" }}
         >
           I am a React Apprentice at Alphaworks (a subsidiary of Bitwise
           Industries). I am incredibly curious and have a huge thirst for
